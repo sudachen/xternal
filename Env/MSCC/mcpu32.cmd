4d6d8d095b205328dcd15ae584b3e6cb696eb101
Alexey Sudachen <alexey@sudachen.name>
2015-06-05 10:14:52 -0300

    MSCC scripts added

---
+++ Env/MSCC/mcpu32.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+if not "%VS120COMNTOOLS%" == "" goto callVCenv
+if .%PROCESSOR_ARCHITECTURE%. == .x86. set VS120COMNTOOLS="%ProgramFiles%\Microsoft Visual Studio 12.0\Common7\Tools\" 
+if not .%PROCESSOR_ARCHITECTURE%. == .x86. set VS120COMNTOOLS="%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\Common7\Tools\" 
+
+:callVCenv
+
+call "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
+
+set TARGET_CPU=X86
+set TARGET_INFIX=_32r12
+

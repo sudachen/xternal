fac3c7d523169e3e2ab11fbc95f47665cbabe6fe
Alexey Sudachen <alexey@sudachen.name>
2017-07-10 05:13:10 -0400

    updated

--- Env/env32_140.cmd
+++ Env/env32_140.cmd
@@ -1,8 +1,8 @@
 @echo off
 
 if not "%VS140COMNTOOLS%" == "" goto callVCenv
-if .%PROCESSOR_ARCHITECTURE%. == .x86. set VS140COMNTOOLS="%ProgramFiles%\Microsoft Visual Studio 14.0\Common7\Tools\" 
-if not .%PROCESSOR_ARCHITECTURE%. == .x86. set VS140COMNTOOLS="%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\" 
+if .%PROCESSOR_ARCHITECTURE%. == .x86. set VS140COMNTOOLS=%ProgramFiles%\Microsoft Visual Studio 14.0\Common7\Tools\ 
+if not .%PROCESSOR_ARCHITECTURE%. == .x86. set VS140COMNTOOLS=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\ 
 
 :callVCenv
 
--- Env/env64_140.cmd
+++ Env/env64_140.cmd
@@ -1,14 +1,13 @@
 @echo off
 
 if not "%VS140COMNTOOLS%" == "" goto callVCenv
-if .%PROCESSOR_ARCHITECTURE%. == .x86. set VS140COMNTOOLS="%ProgramFiles%\Microsoft Visual Studio 14.0\Common7\Tools\" 
-if not .%PROCESSOR_ARCHITECTURE%. == .x86. set VS140COMNTOOLS="%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\" 
+if .%PROCESSOR_ARCHITECTURE%. == .x86. set VS140COMNTOOLS=%ProgramFiles%\Microsoft Visual Studio 14.0\Common7\Tools\ 
+if not .%PROCESSOR_ARCHITECTURE%. == .x86. set VS140COMNTOOLS=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\ 
 
 :callVCenv
 
-if .%PROCESSOR_ARCHITECTURE%. == .x86. call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
-if .%PROCESSOR_ARCHITECTURE%. == .x86. call "%VS140COMNTOOLS%\..\..\VC\bin\x86_amd64\vcvarsx86_amd64.bat" x86_amd64
-if not .%PROCESSOR_ARCHITECTURE%. == .x86. call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" amd64
+if .%PROCESSOR_ARCHITECTURE%. == .x86. call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x86_amd64
+if not .%PROCESSOR_ARCHITECTURE%. == .x86. call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" amd64
 
 set TARGET_CPU=X64
 set TARGET_INFIX=_64r14
